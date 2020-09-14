dc19dcd49a28697f56820f93ddda867972ef3e38
Sandra Guetg <guetgs@gmail.com>
2020-09-14 14:21:59 +0200

    Add description

--- DatabaseToDataFrame.py
+++ DatabaseToDataFrame.py
@@ -1,23 +1,31 @@
 import mariadb
 import sys
 import pandas as pd
-import numpy as np
+
 
 def DatabaseToDataFrame(cols, table_statement):
-	try:
-		conn = mariadb.connect(
-		user="python",
-		host="localhost")
-	except mariadb.Error as e:
-		print(f"Error connecting to MariaDB Platform: {e}")
-		sys.exit(1)
-		
-	cur = conn.cursor()
+    '''
+    INPUT: list, text
+    OUTPUT: pandas DataFrame
 
-	SQLstatement = "SELECT " + ", ".join(cols) +  " FROM " + table_statement
-	cur.execute(SQLstatement)
-	df = pd.DataFrame([value for value in cur], columns=cols)
+    Connects to a MariaDB database on localhost using the user "python" and
+    pulls data specified by the input columns from table(s) defined in
+    table_statement. Returns the data as a pandas DataFrame.
+    '''
+    try:
+        conn = mariadb.connect(
+            user="python",
+            host="localhost")
+    except mariadb.Error as e:
+        print(f"Error connecting to MariaDB Platform: {e}")
+        sys.exit(1)
 
-	conn.close()
+    cur = conn.cursor()
 
-	return df
+    SQLstatement = "SELECT " + ", ".join(cols) + " FROM " + table_statement
+    cur.execute(SQLstatement)
+    df = pd.DataFrame([value for value in cur], columns=cols)
+
+    conn.close()
+
+    return df
