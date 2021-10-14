507cfb8f4bd308c1f22be762d98a8bb23c309849
Sandra Guetg <guetgs@gmail.com>
2021-10-14 12:50:28 +0200

    improve functions

--- Analysis.py
+++ Analysis.py
@@ -1,15 +1,14 @@
 import pandas as pd
 
-def getIndividualFrames(zahler, readings, 
-                        zahler_name='zahler_name', 
-                        readings_name='name'):
-    meters = zahler[zahler_name].unique()
+def getIndividualFrames(readings,  
+                        col_name='name'):
+    meters = readings[col_name].unique()
     dfs = {}
     for meter in meters:
-        df = readings[readings[readings_name] == meter].copy()
+        df = readings[readings[col_name] == meter].copy()
         df['date'] = pd.to_datetime(df['date'])
         df = df.set_index('date')
-        df = df.drop(readings_name, axis=1)
+        df = df.drop(col_name, axis=1)
         dfs[meter] = df
     return dfs
 
@@ -17,6 +16,5 @@ def getIntervalData(meter_readings, interval):
     df = meter_readings.resample('1d').interpolate()
     resampler = df.resample(interval, label='left')
     starts = resampler.first()
-    ends = resampler.last()
-
-    return ends[:-1]-starts[:-1]
+    ends = starts[1:].values
+    return starts[:-1]*-1 + ends
