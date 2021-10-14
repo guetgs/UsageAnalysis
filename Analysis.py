888b5d9164d7be660263d80b5239ca6faaa7a0a4
Sandra Guetg <guetgs@gmail.com>
2021-10-14 13:41:28 +0200

    add initial function to estimate costs

--- Analysis.py
+++ Analysis.py
@@ -21,4 +21,14 @@ def getIntervalData(meter_readings, interval):
 
 def getAverageConsumption(meter_readings, interval):
     interval_data = getIntervalData(meter_readings, interval)
-    return interval_data.mean()
+    return interval_data.mean().values[0]
+
+def getMonthlyCost(utility, readings, prices):
+    gas_conversion = 0.9674 * 11.2920
+    meter_readings = getIndividualFrames(readings)[utility]
+    if utility == 'Gas':
+        meter_readings = meter_readings * gas_conversion
+    yearly = getAverageConsumption(meter_readings, '365d')
+    ut_price = prices[utility]
+    yearly_cost = ut_price[1]*12 + ut_price[0]*yearly
+    return yearly_cost/12
